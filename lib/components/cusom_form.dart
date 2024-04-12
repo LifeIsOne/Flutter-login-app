7f2814fcba47a33ba79ff2be48513effb08ced11
LifeIsOne <didtmdgh258@gmail.com>
2024-04-12 17:34:39 +0900

    InputDecoration 재사용

--- lib/components/cusom_form.dart
+++ lib/components/cusom_form.dart
@@ -1,36 +1,29 @@
 import 'package:flutter/material.dart';
 
-class CustomForm extends StatelessWidget {
-  const CustomForm({
-    super.key,
-  });
+import 'custom_text_form_field.dart';
 
+class CustomForm extends StatelessWidget {
+  final formkey = GlobalKey<FormState>();
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Form(
+      key: formkey,
       child: Column(
         children: [
-          Text("Email"),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "Enter Email",
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-          ),
+          CustomTextFormField(controller: email, text: "Email"),
+          CustomTextFormField(controller: password, text: "Password"),
           TextButton(
-            onPressed: () {},
-            child: Text("Log-in"),
-          )
+            onPressed: () {
+              //print("email : ${email.text}");
+              if (formkey.currentState!.validate()) {
+                Navigator.pushNamed(context, "/home");
+              }
+            },
+            child: Text("Login"),
+          ),
         ],
       ),
     );
---
+++ lib/components/custom_text_form_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final controller;
+  final text;
+
+  CustomTextFormField({required this.controller, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Email"),
+        TextFormField(
+          validator: (value) {
+            print("value : ${value}");
+
+            if (value!.isEmpty) {
+              return "비어있을수 없습니다";
+            } else {
+              return null; // 정상일때 null을 리턴한다.
+            }
+          },
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: "Enter ${text}",
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
