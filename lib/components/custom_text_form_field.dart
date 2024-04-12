8d8d194257bc9a90696633d544f564fe6caaf791
LifeIsOne <didtmdgh258@gmail.com>
2024-04-12 17:36:49 +0900

    InputDecoration 구현 완료

--- lib/components/custom_text_form_field.dart
+++ lib/components/custom_text_form_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_app/size.dart';
 
 class CustomTextFormField extends StatelessWidget {
   final controller;
@@ -8,37 +9,41 @@ class CustomTextFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Email"),
-        TextFormField(
-          validator: (value) {
-            print("value : ${value}");
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: mediumGap),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Email"),
+          TextFormField(
+            validator: (value) {
+              print("value : ${value}");
 
-            if (value!.isEmpty) {
-              return "비어있을수 없습니다";
-            } else {
-              return null; // 정상일때 null을 리턴한다.
-            }
-          },
-          controller: controller,
-          decoration: InputDecoration(
-            hintText: "Enter ${text}",
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
+              if (value!.isEmpty) {
+                return "비어있을수 없습니다";
+              } else {
+                return null; // 정상일때 null을 리턴한다.
+              }
+            },
+            controller: controller,
+            decoration: InputDecoration(
+              hintText: "Enter ${text}",
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
